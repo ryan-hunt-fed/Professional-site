a38d333c8821d3e9290b98319d51bea41ed8731f
Ryan Hunt <r-g-hunt@hotmail.com>
2023-10-30 12:14:11 +1300

    added career interests

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -142,7 +142,7 @@ function Home() {
                     <p>Areas interetsed in but would need some more training and/or on the job experirence.</p>
                     <ul className='ideas-list'>
                         <li>UX Deisgner</li>
-                        <li>3D Printing</li>
+                        <li>3D Printer</li>
                         <li>Unreal Engine Developer/Designer</li>
                         <li>Webflow Designer</li>
                         <li>Photo Editor</li>
